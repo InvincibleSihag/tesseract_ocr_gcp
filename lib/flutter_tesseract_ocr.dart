2734810b51bfdad26752408c231f80cc25f1947c
Sawan Sihag <75479923+InvincibleSihag@users.noreply.github.com>
2024-03-19 23:39:11 +0530

    removed web support

--- lib/flutter_tesseract_ocr.dart
+++ lib/flutter_tesseract_ocr.dart
@@ -1,4 +1,4 @@
 library flutter_tesseract_ocr;
 
-export 'android_ios.dart' // Stub implementation
-    if (dart.library.html) 'web.dart'; // dart:html implementation
+export 'android_ios.dart';// Stub implementation
+// if (dart.library.html) 'web.dart'; // dart:html implementation
